0699bd54939f43646bf1aeb0409a7eecc3b6622d
tuccster <coleslater50@gmail.com>
2020-12-11 22:20:37 -0800

    20w50c
    added menu scene
    added ability to connect to server

---
+++ Assets/Scripts/ServerLauncher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Lemon
+{
+    public class ServerLauncher : MonoBehaviourPunCallbacks
+    {
+        // This should be moved to a more general game information class
+        private static string gameVersion = "20w50b";
+
+        public Text networkStatus;
+
+        private void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+        public override void OnConnectedToMaster()
+		{
+            networkStatus.text = "Status: Connected to master";
+            PhotonNetwork.JoinRandomRoom();
+		}
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {   
+            networkStatus.text = "Status: Unable to join room";
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+        }
+
+        public override void OnJoinedRoom()
+        {
+            networkStatus.text = "Status: Joined room";
+
+            if (PhotonNetwork.IsMasterClient)
+                PhotonNetwork.LoadLevel(1);
+        }
+    }
+}
---
+++ Assets/Scripts/ServerSceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Lemon
+{
+    public class ServerSceneManager : MonoBehaviourPunCallbacks
+    {   
+        public Text masterClient;
+
+        private void Start()
+        {
+            masterClient.text = "master_client : " + PhotonNetwork.IsMasterClient + "\napp_id : " + Application.identifier;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+                PhotonNetwork.LeaveRoom();
+        }
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+                return;
+            }
+
+            Debug.LogFormat("PhotonNetwork : Loading Level : dev");
+            PhotonNetwork.LoadLevel("dev");
+        }
+    }
+}
